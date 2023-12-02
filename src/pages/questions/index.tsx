f6f873fcf1afba44a66830a7f0204d65acc642f3
kjj <playck23@gmail.com>
2023-12-02 18:25:43 +0900

    [Question] 문제 리스트 UI

--- src/pages/questions/index.tsx
+++ src/pages/questions/index.tsx
@@ -1,9 +1,33 @@
-import { Flex } from '@chakra-ui/react'
-
+import { Center, Grid, GridItem } from '@chakra-ui/react'
+import Link from 'next/link'
 interface QuestionsListProps {}
 
 function QuestionsList(props: QuestionsListProps) {
-  return <Flex></Flex>
+  const arr = [1, 2, 3, 4, 5]
+
+  return (
+    <Grid templateColumns="repeat(3, 1fr)" gap={6} p="30px 20px">
+      {arr.map((el) => (
+        <Link key={el} href={`questions/${el}`}>
+          <GridItem
+            key={el}
+            display="flex"
+            flex="1 1 0"
+            justifyContent="center"
+            alignItems="center"
+            aspectRatio="1 / 1"
+            bg="#FFF"
+            borderRadius="10px"
+            color="gray.700"
+            cursor="pointer"
+            boxShadow="0px 3px 5px rgba(0, 0, 0, 0.12)"
+          >
+            <Center>{el}</Center>
+          </GridItem>
+        </Link>
+      ))}
+    </Grid>
+  )
 }
 
 export default QuestionsList
