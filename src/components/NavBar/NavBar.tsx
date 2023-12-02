fac80bf29194c62d42349176b3cea0558d16b834
kjj <playck23@gmail.com>
2023-12-02 18:43:14 +0900

    [Nav] 뒤로가기 버튼 구현

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -1,8 +1,12 @@
 import { Flex } from '@chakra-ui/react'
+import { useRouter } from 'next/router'
+import { FaAngleLeft } from 'react-icons/fa6'
 
 interface NavBarProps {}
 
 function NavBar(props: NavBarProps) {
+  const router = useRouter()
+
   return (
     <>
       <Flex
@@ -15,7 +19,9 @@ function NavBar(props: NavBarProps) {
         borderBottom="1px solid"
         borderBottomColor="gray.10"
       >
-        <Flex>-</Flex>
+        <Flex justifyContent="space-between" cursor="pointer" py="20px" pr="20px" onClick={() => router.back()}>
+          <FaAngleLeft size={20} />
+        </Flex>
       </Flex>
     </>
   )
