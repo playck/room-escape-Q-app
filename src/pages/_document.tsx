d47f2a39dd964ed0988463f3c155b76eab69488a
kjj <playck23@gmail.com>
2024-03-14 02:31:28 +0900

    google AdSense script code 삽입

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,11 @@ export default function Document() {
     <Html lang="ko">
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=Black+Han+Sans&display=swap" rel="stylesheet" />
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6018563398084009"
+          crossOrigin="anonymous"
+        ></script>
       </Head>
       <body>
         <Main />
